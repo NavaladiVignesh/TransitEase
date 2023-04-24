028d804578c09b69b490ed3be4f375cb3d097d4e
Vignesh Navaladi <65125144+NavaladiVignesh@users.noreply.github.com>
2023-04-24 14:51:44 +0530

    Add files via upload

---
+++ lib/pages/map_display.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class MapDisplay extends StatefulWidget {
+  const MapDisplay({super.key});
+
+  @override
+  State<MapDisplay> createState() => _MapDisplayState();
+}
+
+class _MapDisplayState extends State<MapDisplay> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
